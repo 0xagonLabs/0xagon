256b0be2e7df7911b580cb21642911c561cb117c
Gill <asguilift@hotmail.com>
2022-06-20 20:22:09 -0300

    Static

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
+// module.exports = withBundleAnalyzer({
+//   poweredByHeader: false,
+//   trailingSlash: true,
+//   basePath: '',
+//   reactStrictMode: true,
+// });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-});
+  assetPrefix: './',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+};
+
+module.exports = nextConfig;
