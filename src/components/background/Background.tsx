b2c5f895ddbee7d86949a3429db1c6147bd99109
Gill <asguilift@hotmail.com>
2022-06-24 15:09:42 -0300

    Changing % to vh

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -20,8 +20,8 @@ const Background = (props: IBackgroundProps) => {
         {`
           #clouds {
             position: absolute;
-            width: 100%;
-            height: 100%;
+            width: 100vh;
+            height: 100vh;
             background: #c9dbe9;
             background: -linear-gradient(top, #c9dbe9 0%, #fff 100%);
             background-image: linear-gradient(90deg, #c9dbe9 10%, #fff 100%);
